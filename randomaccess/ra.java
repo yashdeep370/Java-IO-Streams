429f6ac334f37db962859f2e12382f7cddd78949
Yash Deep <143354431+yashdeep370@users.noreply.github.com>
2025-03-20 00:26:32 +0530

    Add files via upload

---
+++ randomaccess/ra.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class ra 
+{
+    public static void main(String[] args) throws Exception
+    {
+        RandomAccessFile rf=new RandomAccessFile("C:\\MyJava\\Data.txt","rw");
+       //byte b[]={'A','B','C','D','E','F','G''H','I','J'};
+       
+        System.out.println((char)rf.read());
+        System.out.println((char)rf.read());
+        System.out.println((char)rf.read());
+        rf.write('d');
+        System.out.println((char)rf.read());
+        rf.skipBytes(3);
+        System.out.println((char)rf.read());
+        rf.seek(3);
+        System.out.println((char)rf.read());
+        System.out.println(rf.getFilePointer());
+        rf.seek(rf.getFilePointer()+2);
+        System.out.println((char)rf.read());    
+    }   
+}
